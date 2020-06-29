a9ceaa2b3c1ac0b5093cd73751ebc106a105644c
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-29 21:01:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,13 +6,30 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
 
   end
 
+  def songs
 
+    Song.all.select do |song|
+        song.artist == self
 
 end
+
+def add_song(song)
+song.artist = self
+@songs << song
+  end
+
+end
+end
+
+
+
+
+
+
+
 
 
 
